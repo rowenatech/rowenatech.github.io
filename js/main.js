76631c67fb5ab4f3275eb12b3b7312597f5e261c
yankun.xie <yankun.xie@ktctc.com>
2015-11-05 18:26:00 +0800

    lwy

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by John on 15/11/4.
+ */
+$(document).ready(function(){
+    //alert($(window).height());
+    var winHeight = $(window).height();
+    $('#home').height(winHeight);
+    $('#service').height(winHeight);
+    $('#aboutus').height(winHeight);
+    $('#contact').height(winHeight);
+})
